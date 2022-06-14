58fc34f72570992fdc5f2778822cb5c54a76dfdb
mickiy123 <mikikinfe345@gmail.com>
2022-06-14 20:38:37 +0300

    Use differnt approach

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,8 +1,4 @@
 export default function returnHowManyArguments(...theArgs) {
-        let count = 0;
 
-        for (const arg of theArgs) {
-                count = count + 1;
-        }
-        return count;
+	return theArgs.length;
 }
