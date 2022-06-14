c1a3dda7abc9235ebd7291ecf946485b7bc3ace3
mickiy123 <mikikinfe345@gmail.com>
2022-06-14 19:13:22 +0300

    Bug fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,14 @@
 export default function taskBlock(trueOrFalse) {
   var task = false;
-   var task2 = true;
+  var task2 = true;
 
   if (trueOrFalse) {
-    var task2 = true;
-    var task = false;
+    var task = true;
+    var task2 = false;
   }
+
+  var task = false;
+  var task2 = true;
 
   return [task, task2];
 }
