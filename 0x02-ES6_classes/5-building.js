9f7916471417295709af89ae3685bcf83ed85338
mickiy123 <mikikinfe345@gmail.com>
2022-06-16 23:52:28 +0300

    Complete task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,6 +2,12 @@ export default class Building {
   constructor(sqft) {
     /* eslint no-underscore-dangle: ["error", { "allow": ["_sqft"] }] */
     this._sqft = sqft;
+    const proto = Object.getPrototypeOf(this);
+    const superProto = Building.prototype;
+    /* eslint no-prototype-builtins: "error" */
+    const missing = Object.getOwnPropertyNames(superProto).find((name) => typeof superProto[name] === 'function' && !Object.prototype.hasOwnProperty.call(proto, name));
+
+    if (missing) throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 
   get sqft() {
@@ -10,5 +16,9 @@ export default class Building {
 
   set sqft(value) {
     this._sqft = value;
+  }
+
+  evacuationWarningMessage() {
+    return `evacuationWarningMessage ${this._sqft}`;
   }
 }
