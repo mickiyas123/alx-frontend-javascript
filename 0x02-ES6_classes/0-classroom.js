c5e4bad0d550ce7a92db9b79b45361c9bcf3da4c
mickiy123 <mikikinfe345@gmail.com>
2022-06-16 13:54:28 +0300

    Complete task 0

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    /* eslint no-underscore-dangle: ["error", { "allow": ["_maxStudentsSize"] }] */
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
