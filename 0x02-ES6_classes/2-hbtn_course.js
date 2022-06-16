2799658dee09551186195bb3554d5c0bf2d9e0d5
mickiy123 <mikikinfe345@gmail.com>
2022-06-16 14:53:47 +0300

    Complete task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,47 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    /* eslint no-underscore-dangle: ["error", { "allow": ["_name", "_length", "_students"] }] */
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    if (typeof students !== 'object') {
+      throw new TypeError('Students must be an object');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    }
+  }
+
+  set length(value) {
+    if (typeof value === 'number') {
+      this._length = value;
+    }
+  }
+
+  set students(value) {
+    if (typeof value === 'object') {
+      this._students = value;
+    }
+  }
+}
