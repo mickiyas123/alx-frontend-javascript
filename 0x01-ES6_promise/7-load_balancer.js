6272fc344bce49b4c407da94552014de181550a3
mickiy123 <mikikinfe345@gmail.com>
2022-06-18 13:14:54 +0300

    Fix bug

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,4 +1,4 @@
 export default function loadBalancer(chinaDownload, USDownload) {
-  return Promise.any([chinaDownload, USDownload])
+  return Promise.race([chinaDownload, USDownload])
     .then((value) => value);
 }
