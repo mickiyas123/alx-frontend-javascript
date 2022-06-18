f4c46c03033d313dfe95544dc8c410cedbaa5787
mickiy123 <mikikinfe345@gmail.com>
2022-06-18 11:42:28 +0300

    Complete task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,9 +2,10 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUp = signUpUser(fileName);
-  const upload = uploadPhoto(firstName, lastName);
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
 
-  return Promise.all([signUp, upload])
-    .then((values) => console.log(values)).catch(() => console.log());
+  // Promise.allSettled([user, photo]).then(messages => messages)
+  return Promise.allSettled([user, photo])
+    .then((result) => result);
 }
