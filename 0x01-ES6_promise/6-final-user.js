18f5015aa4c4c8e06b3181746d7b0180957db7c4
mickiy123 <mikikinfe345@gmail.com>
2022-06-18 12:56:52 +0300

    Complete task6(use an array)

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,7 +5,16 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
 
-  // Promise.allSettled([user, photo]).then(messages => messages)
   return Promise.allSettled([user, photo])
-    .then((result) => result);
+    .then((results) => {
+      const resultArray = [];
+      results.forEach((result) => {
+        if (result.status === 'fulfilled') {
+          resultArray.push({ status: result.status, value: result.value });
+        } else {
+          resultArray.push({ status: result.status, value: `${result.reason}` });
+        }
+      });
+      return resultArray;
+    });
 }
