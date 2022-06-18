d30b46a3599c5ee18a0264b624c7a8c8035bffc5
mickiy123 <mikikinfe345@gmail.com>
2022-06-18 10:42:18 +0300

    Change code style

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  const obj = { status: 200, body: 'success' };
-
-  return promise.then(() => obj).catch(() => new Error()).finally(() => console.log('Got a response from the API'));
+  promise.then(() => { 'status: 200, body: success'; })
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
