0584f72b8186be47f80974a50c67b9a2d8677f4a
mickiy123 <mikikinfe345@gmail.com>
2022-06-18 11:07:34 +0300

    Fix bug(add return keyword)

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  const obj = { status: 200, body: 'success' };
-  promise.then(() => obj)
+  return promise.then(() => { 'status: 200, body: success'; })
     .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
