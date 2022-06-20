dda11e1fbf1d84061073716cbe2333e88f3624df
mickiy123 <mikikinfe345@gmail.com>
2022-06-20 19:32:55 +0300

    Add condition

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,13 +1,12 @@
 export default function updateUniqueItems(map) {
   if (typeof map !== 'object') {
-    return 'Cannot process';
-  }
-
-  for (const [key, val] of map) {
-    if (val === 1) {
-      map.set(key, 100);
+    console.log('Cannot process');
+  } else {
+    for (const [key, val] of map) {
+      if (val === 1) {
+        map.set(key, 100);
+      }
     }
   }
-
   return map;
 }
