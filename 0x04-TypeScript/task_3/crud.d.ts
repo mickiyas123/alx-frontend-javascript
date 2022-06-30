7ccf92452de364cae657f0553116ba88470a4030
mickiy123 <mikikinfe345@gmail.com>
2022-06-30 16:24:54 +0300

    Remove files

--- 0x04-TypeScript/task_3/crud.d.ts
+++
@@ -1,7 +0,0 @@
-import { RowID, RowElement } from './interface';
-
-export declare function insertRow(row: RowElement): RowID;
-
-export declare function deleteRow(rowId: RowID): void;
-
-export declare function updateRow(rowId: RowID, row: RowElement): RowID;
